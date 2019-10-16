9b55ae0645905f52d38f1a6651bfd77fa30e10d8
André Monello <atmonello@gmail.com>
2019-10-15 23:07:08 -0300

    Adicionando criação de usuário e hash de senha

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'André Monello',
-    email: 'atmonello@outlook.com',
-    password_hash: '123412512',
-  });
-
-  res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
