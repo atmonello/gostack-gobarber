fda7cae73bf68a33a4c70658ed62a03a3273f451
André Monello <atmonello@gmail.com>
2019-10-15 22:49:15 -0300

    Criando loader de models

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello world!' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'André Monello',
+    email: 'atmonello@outlook.com',
+    password_hash: '123412512',
+  });
+
+  res.json(user);
+});
 
 export default routes;
