b28aff7960227914e9ac04de10a4b06af9647906
André Monello <atmonello@gmail.com>
2019-10-22 21:07:23 -0300

    Retornando campos virtuais no agendamento

--- backend/src/app/controllers/AppointmentController.js
+++ backend/src/app/controllers/AppointmentController.js
@@ -6,7 +6,6 @@ import User from '../models/User';
 import File from '../models/File';
 import Notification from '../schemas/Notification';
 
-// import Mail from '../../lib/Mail';
 import Queue from '../../lib/Queue';
 import CancellationMail from '../jobs/CancellationMail';
 
@@ -107,7 +106,7 @@ class AppointmentController {
       order: [['date', 'DESC']],
       limit: 20,
       offset: (page - 1) * 20,
-      attributes: ['id', 'date', 'cancelled_at'],
+      attributes: ['id', 'date', 'cancelled_at', 'past', 'cancelable'],
       include: [
         {
           model: User,
@@ -170,19 +169,6 @@ class AppointmentController {
     await appointment.save();
 
     await Queue.add(CancellationMail.key, { appointment });
-
-    // await Mail.sendMail({
-    //   to: `${appointment.provider.name} <${appointment.provider.email}>`,
-    //   subject: 'Agendamento cancelado',
-    //   template: 'cancellation',
-    //   context: {
-    //     provider: appointment.provider.name,
-    //     user: appointment.user.name,
-    //     date: format(appointment.date, "'dia' dd 'de' MMMM', às 'H:mm'h'", {
-    //       locale: ptBR,
-    //     }),
-    //   },
-    // });
 
     return res.json(appointment);
   }
